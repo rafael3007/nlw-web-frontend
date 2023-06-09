c17da360e7599328ebd9ed9e0c10319d6d290384
= <=>
2023-06-08 23:19:56 -0300

    authentication with routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
+import Copyright from '@/components/Copytigth'
+import Hero from '@/components/Hero'
+import Profile from '@/components/Profile'
+import Signin from '@/components/Signin'
 import {
   Bai_Jamjuree as BaiJamjuree,
   Roboto_Flex as Roboto,
 } from 'next/font/google'
+import { cookies } from 'next/headers'
+import React from 'react'
 import './globals.css'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
@@ -22,12 +28,35 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/** Left */}
+          <div className=" relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
+            {/** Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-0 top-0 w-2 bg-Stripes"></div>
+            {/** Sign In */}
+
+            {isAuthenticated ? <Profile /> : <Signin />}
+
+            {/** Hero */}
+            <Hero />
+
+            {/** Copyright */}
+            <Copyright />
+          </div>
+          {/** Direita */}
+          <div className="bg-{url(../assets/bg-stars.svg)} flex flex-col bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
